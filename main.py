ceade6b40ad7f68605fb7333fdf033de03de5946
nathbvernucci <larissa.cirilo@hotmail.com>
2025-04-16 12:28:28 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -185,4 +185,4 @@ async def main():
     app.add_handler(CommandHandler("cargo", cargo))
     app.add_handler(CallbackQueryHandler(definir_cargo, pattern='^[A-Za-z ]+$'))
     app.add_handler(CommandHandler("sala_vip", sala_vip))
-    app.add_handler(CallbackQueryHandler(entrar_vip, pattern='entrar
+    app.add_handler(CallbackQueryHandler(entrar_vip, pattern='entrar))
